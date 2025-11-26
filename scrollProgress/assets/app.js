956c5be668eced279a586945785e7cc59f236b59
Robin Devouge <robin.devouge@gmail.com>
2025-11-26 08:59:08 +0100

    [scrollProgress] Lint files

--- scrollProgress/assets/app.js
+++ scrollProgress/assets/app.js
@@ -1,16 +1,16 @@
-"use strict";
+'use strict';
 
-const progressBar = document.querySelector(".scrollbar__progress");
+const progressBar = document.querySelector('.scrollbar__progress');
 
-if(progressBar){
-    window.addEventListener("scroll", updateScrollProgress);
+if (progressBar) {
+	window.addEventListener('scroll', updateScrollProgress);
 }
 
-function updateScrollProgress(event){
-    const maxScroll = document.body.scrollHeight - window.innerHeight;
-    const currentScroll = window.scrollY;
-    const readPercentage = (currentScroll / maxScroll) * 100;
-    if(currentScroll >= 0){
-        progressBar.style.setProperty('--scrollbar-width', readPercentage + '%');
-    }
+function updateScrollProgress(event) {
+	const maxScroll = document.body.scrollHeight - window.innerHeight;
+	const currentScroll = window.scrollY;
+	const readPercentage = (currentScroll / maxScroll) * 100;
+	if (currentScroll >= 0) {
+		progressBar.style.setProperty('--scrollbar-width', readPercentage + '%');
+	}
 }
