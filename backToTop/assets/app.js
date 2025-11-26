ae93dbc1299088c8823681f6200ffa4daacf9ce1
Robin Devouge <robin.devouge@gmail.com>
2025-11-26 08:58:55 +0100

    [backToTop] Lint files

--- backToTop/assets/app.js
+++ backToTop/assets/app.js
@@ -1,16 +1,16 @@
-"use strict";
+'use strict';
 
-const backTop = document.querySelector(".backTop");
+const backTop = document.querySelector('.backTop');
 
-if(backTop){
-    window.addEventListener("scroll", scrollListener);
+if (backTop) {
+	window.addEventListener('scroll', scrollListener);
 }
 
-function scrollListener(event){
-    const currentScroll = window.scrollY;
-    if(currentScroll > 300){
-        backTop.classList.add("backTop--show");
-    }else{
-        backTop.classList.remove("backTop--show");
-    }    
+function scrollListener(event) {
+	const currentScroll = window.scrollY;
+	if (currentScroll > 300) {
+		backTop.classList.add('backTop--show');
+	} else {
+		backTop.classList.remove('backTop--show');
+	}
 }
